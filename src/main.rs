85a1b0cbd8b58473e82346b2b748370e9f5c5ad7
Rodrigo Lorenzo Leal <code@rlorenzoleal.com>
2026-01-27 14:41:53 +0100

    feat: add basic health check endpoint

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:800").expect("Failed to bind to port");
+    run(listener)?.await
+}
