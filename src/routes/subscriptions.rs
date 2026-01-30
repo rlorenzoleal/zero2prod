a0f24f745ae73c1b668a99571f46485135c20b06
Rodrigo Lorenzo Leal <code@rlorenzoleal.com>
2026-01-30 20:46:24 +0100

    refactor: restructure folders and source files

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,12 @@
+use actix_web::{HttpResponse, web};
+
+#[derive(serde::Deserialize)]
+#[allow(dead_code)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
