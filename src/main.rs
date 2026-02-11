9489c246a29ff41e541ff28f03db1c550a0ebfb5
Rodrigo Lorenzo Leal <code@rlorenzoleal.com>
2026-02-11 18:39:13 +0100

    refactor: improve startup logic and restructure tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-#[allow(unused_imports)]
-use sqlx::Connection;
-use sqlx::PgPool;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -13,25 +8,8 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
+    let application = Application::build(configuration).await?;
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let listener = TcpListener::bind(address).expect("Failed to bind to port");
-    run(listener, connection_pool, email_client)?.await
+    application.run_until_stopped().await?;
+    Ok(())
 }
