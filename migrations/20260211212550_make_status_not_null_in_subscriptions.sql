81b92f8e75979a08654f78b7f8c0ab55367378f8
Rodrigo Lorenzo Leal <code@rlorenzoleal.com>
2026-02-11 22:32:11 +0100

    feat(db): backfill status and add NOT NULL constraint

---
+++ migrations/20260211212550_make_status_not_null_in_subscriptions.sql
@@ -0,0 +1,14 @@
+-- Backfill Status
+BEGIN;
+-- Backfill `status` for historical entries
+UPDATE
+    subscriptions
+SET
+    status = 'confirmed'
+WHERE
+    status IS NULL;
+-- Make `status` mandatory
+ALTER TABLE subscriptions
+    ALTER COLUMN status SET NOT NULL;
+COMMIT;
+
